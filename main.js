8445fbb50d54a4c40f2c2fd42c730902067328d1
Mohinur <15@02>
2022-12-24 17:53:19 +0500

    save

---
+++ main.js
@@ -0,0 +1,10 @@
+var x = +prompt('Введите первое число:')
+console.log(x);
+var y = +prompt('Введите второе число:');
+console.log(y);
+var z = +prompt('Введите третье число:')
+console.log(z);
+let result = (x + y + z) / 3
+alert('Среднее арифметическое чисел = ' + result)
+
+
